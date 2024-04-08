27fc50b6777bdc75a1c4a8e88e9338c18058c942
Jan Jörg <jan.joerg.jj@gmail.com>
2024-04-08 15:50:38 +0200

    redesign

---
+++ src/lib/client/navbar.ts
@@ -0,0 +1,6 @@
+export const titles = [
+	{ name: 'My Work', path: 'https://www.jan-joerg.ch/projects', title: 'Projects' },
+	{ name: 'NPM Packages', path: '/', title: 'NPM Packages' }
+];
+
+export const npmPackages = [{ name: 'svelte-lightbox-gallery', path: '/svelte-lightbox-gallery' }];
---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import { Progress } from '$lib/components/ui/progress';
+
+	let value = 0;
+
+	onMount(() => {
+		setInterval(() => {
+			value++;
+		}, 10);
+	});
+
+	$: {
+		if (value == 110) {
+			window.location.href = '/';
+		}
+	}
+</script>
+
+<h1>{$page.status}</h1>
+<p>{$page.error.message}</p>
+<div class="flex justify-center items-center px-10 pt-5">
+	<Progress {value} max={100} class="max-w-[500px] h-3 bg-gray-700" />
+</div>
